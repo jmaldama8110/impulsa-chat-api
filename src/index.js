677b68e161e6116c065d75547d002135f15f68bf
Josman <josemanuek@hotmail.com>
2023-03-07 11:22:52 -0500

    build scripts and tsc

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('Hello from typscript...');
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const httpServer = (0, http_1.createServer)();
+const port = process.env.PORT || 4076;
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+io.on("connection", (socket) => {
+    socket.on('client-message', (data) => {
+        socket.broadcast.emit('server-message', data);
+    });
+});
+httpServer.listen(port, function () {
+    console.log('Socket server started at port ' + port);
+});

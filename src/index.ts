7af26bc8fb18940a0e090a53372da7b77cd7317d
Josman <josemanuek@hotmail.com>
2023-05-27 22:07:22 -0600

    test api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import express from 'express';
 
 const httpServer = createServer();
 const port = process.env.PORT || 4076
+const portApi = process.env.PORT || 3001;
+const app = express();
 
 const io = new Server(httpServer, {
   cors:{
@@ -12,10 +15,22 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
     socket.on('client-message',(data:any)=>{
+    
       socket.broadcast.emit('server-message',data);
     })
   });
 
 httpServer.listen(port, function (){
-    console.log('Socket server started at port ' + port);
+  
+    console.log(`Socket server started at port ${port}`);
 });
+
+
+app.get('/test', (req, res) => {
+  res.send({
+    message: 'Hello from api!'
+  })
+})
+app.listen(portApi, ()=>{
+  console.log('Server started at port: ' + portApi);
+})
